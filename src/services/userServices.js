3fdf40f1f14e87bf754cbad0bf36b9a04c1662a9
Jmyio <jimmyjimmy26282@gmail.com>
2024-05-19 13:37:16 +0800

    v1.4

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -4,15 +4,17 @@ const usersCollection = fbAdmin.db.collection('users');
 
 async function searchUsernames(input) {
     try {
-        // Fetch all users from the collection
         const querySnapshot = await usersCollection.get();
         const users = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 
-        // Use fuzzysort to search usernames
         const results = fuzzysort.go(input, users, { key: 'username' });
 
-        // Return the results with the matched user data
-        return results.map(result => result.obj);
+        const searchResults = [];
+        results.map(result => {
+            delete result.obj.password;
+            searchResults.push(result.obj);
+        });
+        return searchResults;
     } catch (error) {
         console.error('Error searching usernames:', error);
         throw error;
